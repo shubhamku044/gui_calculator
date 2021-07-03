e2ecfdb9c6c4a4d9fe52042edf8f41814dbed86d
shubhamku044 <shubhamku044@gmail.com>
2021-07-03 12:29:43 +0530

    added more functionality

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ from tkinter import *
 root = Tk()
 root.title("Simple Calculator")
 
-e = Entry(root, width=34, borderwidth=3)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 Font_tuple = ("Comic Sans MS", 20, "bold")
 
+equation = StringVar()
+digits = ""
+
+e = Entry(root, width=34, borderwidth=3, textvariable=equation)
+e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+
 def button_click(number):
-    current = e.get()
-    e.delete(0, END)
-    e.insert(0, int(str(current) + str(number)))
+    global digits
+
+    digits = digits + str(number)
+
+    equation.set(digits)
+
 
 def button_clear():
-    e.delete(0, END)
+    global digits
+    digits = ""
+    equation.set("")
 
 def button_add():
     first_number = e.get()
@@ -24,17 +33,16 @@ def button_add():
     e.delete(0, END)
 
 def button_equalto():
-    second_number = e.get()
-    e.delete(0, END)
-    if math == "addition":
-        e.insert(0, f_num + float(second_number))
-    if math == "subtract":
-        e.insert(0, f_num - float(second_number))
-    if math == "multiply":
-        e.insert(0, f_num * float(second_number))
-    if math == "divide":
-        e.insert(0, f_num / float(second_number))
-        
+    try:
+        global digits
+        total = str(eval(digits))
+
+        equation.set(total)
+
+        digits = total
+    except:
+        equation.set("error")
+        digits = ""
 
 
 def subtract():
@@ -63,6 +71,8 @@ def divide():
 
 
 
+
+
 button1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
 button2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
 button3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
@@ -76,10 +86,10 @@ button0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(
 
 button_equal = Button(root, text="=", padx=40, pady=20, fg="red", command=button_equalto)
 button_clear = Button(root, text="C", padx=40, pady=20, fg="red", command=button_clear)
-button_sum = Button(root, text="+", padx=27, pady=20, fg="red", command=button_add)
-button_subtract = Button(root, text="-", padx=30, fg="red", pady=20, command=subtract)
-button_multiply = Button(root, text="x", padx=28, fg="red", pady=20, command=multiply)
-button_divide = Button(root, text="/", padx=30, fg="red", pady=20, command=divide)
+button_sum = Button(root, text="+", padx=27, pady=20, fg="red", command=lambda: button_click("+"))
+button_subtract = Button(root, text="-", padx=30, fg="red", pady=20, command=lambda: button_click("-"))
+button_multiply = Button(root, text="x", padx=28, fg="red", pady=20, command=lambda: button_click("*"))
+button_divide = Button(root, text="/", padx=30, fg="red", pady=20, command=lambda: button_click("/"))
 
 
 
@@ -104,8 +114,6 @@ button_sum.grid(row=1, column=3)
 button_subtract.grid(row=2, column=3)
 button_multiply.grid(row=3, column=3)
 button_divide.grid(row=4, column=3)
-
-
 
 
 root.mainloop()
